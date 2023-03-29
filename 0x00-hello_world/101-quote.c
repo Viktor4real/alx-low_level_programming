ffe59c40b599b1a9b424dddbc045cf410bad7c03
viktor4real <vicomolale@yahoo.com>
2023-03-28 21:40:33 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+#include <unistd.h>
+/**
+ * main - new lin to standard error
+ * Return: 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
